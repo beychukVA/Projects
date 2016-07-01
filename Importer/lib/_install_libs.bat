0513b51b76a209f3ab29f3122958e4e1d5f1740d
drweb777 <beychuk7@gmail.com>
2016-07-01 19:17:17 +0300

    1

--- Importer/lib/_install_libs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Installing libraries...
-
-call mvn install:install-file -Dpackaging=jar -Dversion=1.0.0     -DgroupId=com.x256n.core   -DartifactId=msm-guide-core               -Dfile=msm-guide-core-1.0.0.jar
-
- pause
